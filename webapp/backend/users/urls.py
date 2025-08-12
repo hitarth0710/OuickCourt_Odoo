094af590b6e957708c808241bbb122e847b7a757
Hitarth <hitarthsoni947@gmail.com>
2025-08-12 08:04:55 +0530

    delete old backend

--- webapp/backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/', views.user_profile, name='user-profile'),
-    path('send-otp/', views.send_otp, name='send-otp'),
-    path('verify-otp/', views.verify_otp, name='verify-otp'),
-    path('resend-otp/', views.resend_otp, name='resend-otp'),
-]
